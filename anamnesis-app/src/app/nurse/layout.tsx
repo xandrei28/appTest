4efd0bdcd98c792b51a5306eceefaaf1c8dd59c5
xandrei28 <x.andrei2800@gmail.com>
2026-05-07 18:31:30 +0300

    Delete anamnesis-app directory

--- anamnesis-app/src/app/nurse/layout.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { AppShell } from '@/components/layout/AppShell';
-
-export default function NurseLayout({ children }: { children: React.ReactNode }) {
-  return <AppShell role="nurse">{children}</AppShell>;
-}
--- anamnesis-app/src/app/patient/layout.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { AppShell } from '@/components/layout/AppShell';
-
-export default function PatientLayout({ children }: { children: React.ReactNode }) {
-  return <AppShell role="patient">{children}</AppShell>;
-}
